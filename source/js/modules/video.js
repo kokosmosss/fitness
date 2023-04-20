d3a1f600b2b3b04d3bed5479228e248bb66e8deb
Мария Гаврилова <topik2344@gmail.com>
2023-04-20 16:58:58 +0700

    Добавляет стили для второго блока

---
+++ source/js/modules/video.js
@@ -0,0 +1,14 @@
+const videoBlock = document.querySelector('.gym__video');
+const button = videoBlock.querySelector('.gym__video-button');
+const iframe = videoBlock.querySelector('iframe');
+
+const playVideo = () => {
+  if (videoBlock && iframe) {
+    iframe.style.visibility = 'visible';
+    button.style.display = 'none';
+    // iframe.src += 'autoplay=1';
+    // iframe.setAttribute('allow', 'autoplay');
+  }
+};
+
+button.addEventListener('click', playVideo);
